17f2d63711b4af20335a458d9d27154f41f92e85
Martin Nester <nesterms@dukes.jmu.edu>
2024-07-18 12:55:18 +0000

    add fixed destinations and pullover/go buttons

--- src/main.tsx
+++ src/main.tsx
@@ -24,6 +24,13 @@ let limited_pose = new ROSLIB.Topic({
 	name: '/limited_pose',
 	messageType: 'geometry_msgs/msg/PoseStamped'
 });
+let vehicle_state = new ROSLIB.Topic({
+	ros:ros,
+	name: '/vehicle_state',
+	messageType: 'navigation_interface/msg/VehicleState'
+});
+
+
 
 let protocol = new Protocol();
 maplibregl.addProtocol("pmtiles",protocol.tile);
@@ -33,8 +40,6 @@ let map = new maplibregl.Map({
 	center: [-78.869914,38.435491],
 	zoom: 16,
 });
-
-
 
 let nav = new maplibregl.NavigationControl();
 map.addControl(nav, 'top-left');
@@ -141,11 +146,95 @@ let gps_request = new ROSLIB.Topic({
   messageType : 'navigation_interface/msg/LatLongPoint'
 });
 
+
+function navigateTo(lat,lng){
+	let target = new ROSLIB.Message({
+		latitude: lat,
+		longitude: lng,
+		elevation: 0
+	});
+	gps_request.publish(target);
+
+
+}
+let stop = new ROSLIB.Topic({
+  ros : ros,
+  name : '/stop',
+  messageType : 'navigation_interface/msg/Stop'
+});
+
+
+function setStopped(value){
+	let msg = new ROSLIB.Message({
+		stop: value,
+		distance: 5
+	});
+	stop.publish(msg);
+
+
+}
+// parseFloat([...document.getElementById("destinations").getElementsByTagName("li")][0].getAttribute("lat"))
+
+const goButton = document.getElementById("go");
+const pullOverButton = document.getElementById("pull-over");
+let state = {
+  "is_navigating": false,
+  "reached_destination": true,
+  "stopped": false
+};
+
+[...document.getElementById("destinations").getElementsByTagName("li")].forEach((el)=>{
+	const lat = parseFloat(el.getAttribute("lat"));
+	const long = parseFloat(el.getAttribute("long"));
+	el.addEventListener("click",()=>{
+	
+	console.log(lat,long);
+	if(!state.is_navigating){
+		navigateTo(lat,long);
+		[...document.getElementById("destinations").getElementsByTagName("li")].forEach((el2)=>{
+			el2.classList.remove("selected");
+		});
+
+		el.classList.add("selected");
+		}
+	});
+});
+
+
+pullOverButton.addEventListener("click", ()=>{
+	console.log("PullOver clicked");
+	setStopped(true);
+	
+});
+goButton.addEventListener("click", ()=>{
+	console.log("Go clicked");
+	setStopped(false);
+	
+});
+
+
+vehicle_state.subscribe(function(message){
+	state = message;
+	console.log(message);
+	if(message.reached_destination){
+		
+		map.getSource('remaining_path').setData(LineString([]));
+
+		//map.getSource('visual_path').setData(LineString([]));
+	}
+
+	goButton.hidden=!message.stopped || !message.is_navigating;
+	pullOverButton.hidden=message.stopped || !message.is_navigating;
+
+
+});
+
+
 limited_pose.subscribe(function(message){
 	let [x1,y1] =T(message.pose.position);
 	let source = map.getSource('limited_pose').setData(point(x1,y1));
 
-	if(visual_path_coordinates.length>0){
+	if(visual_path_coordinates.length>0 && state.is_navigating){
 		let closestInd = 0;
 		let closestDist = Infinity;
 		for(let i=0; i < visual_path_coordinates.length ; i++){
@@ -158,44 +247,49 @@ limited_pose.subscribe(function(message){
 		}
 
 		map.getSource('remaining_path').setData(LineString(visual_path_coordinates.slice(closestInd)));
+	
+		map.flyTo({
+			center:[x1,y1],
+			zoom: 19,
+		})
 	}
 });
 
-graph_visual.subscribe(function({markers}) {
-
-	let arrows = markers.filter(({type})=>type==0);
-	map.addSource('graph_visual', {
-		'type':'geojson',
-		'data':{
-			'type':'Feature',
-			'geometry':{
-				'type':'MultiLineString',
-				'coordinates': arrows.map(({points})=>points.map(T))
-			}
-		}
-	});
-	map.addLayer({
-		'id':'graph_visual',
-		'type':'line',
-		'source':'graph_visual',
-		'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#000',
-		'line-opacity': 0.03,
-                "line-width": {
-          "base": 1.4,
-          "stops": [
-            [14, 0],
-            [20, 18]
-          ]
-        }
-            }
-	});
-
-});
+//graph_visual.subscribe(function({markers}) {
+//
+//	let arrows = markers.filter(({type})=>type==0);
+//	map.addSource('graph_visual', {
+//		'type':'geojson',
+//		'data':{
+//			'type':'Feature',
+//			'geometry':{
+//				'type':'MultiLineString',
+//				'coordinates': arrows.map(({points})=>points.map(T))
+//			}
+//		}
+//	});
+//	map.addLayer({
+//		'id':'graph_visual',
+//		'type':'line',
+//		'source':'graph_visual',
+//		'layout': {
+//                'line-join': 'round',
+//                'line-cap': 'round'
+//            },
+//            'paint': {
+//                'line-color': '#000',
+//		'line-opacity': 0.03,
+//                "line-width": {
+//          "base": 1.4,
+//          "stops": [
+//            [14, 0],
+//            [20, 18]
+//          ]
+//        }
+//            }
+//	});
+//
+//});
 map.addLayer({
             'id': 'limited_pose',
             'type': 'symbol',
@@ -204,27 +298,21 @@ map.addLayer({
                 'icon-image': 'custom-marker',
             }
         });
-// When a click event occurs on a feature in the places layer, open a popup at the
-        // location of the feature, with description HTML from its properties.
-        map.on('click', 'graph_visual', (e) => {
-		const {lat,lng} = e.lngLat;
-let target = new ROSLIB.Message({
-latitude: lat,
-longitude: lng,
-elevation: 0
+//// When a click event occurs on a feature in the places layer, open a popup at the
+//        // location of the feature, with description HTML from its properties.
+//        map.on('click', 'graph_visual', (e) => {
+//		const {lat,lng} = e.lngLat;
+//		navigateTo(lat,lng);
+//	    });
+//
+//        // Change the cursor to a pointer when the mouse is over the places layer.
+//        map.on('mouseenter', 'graph_visual', () => {
+//            map.getCanvas().style.cursor = 'pointer';
+//        });
+//
+//        // Change it back to a pointer when it leaves.
+//        map.on('mouseleave', 'graph_visual', () => {
+//            map.getCanvas().style.cursor = '';
+//        });
 });
-gps_request.publish(target);
 
-
-	    });
-
-        // Change the cursor to a pointer when the mouse is over the places layer.
-        map.on('mouseenter', 'graph_visual', () => {
-            map.getCanvas().style.cursor = 'pointer';
-        });
-
-        // Change it back to a pointer when it leaves.
-        map.on('mouseleave', 'graph_visual', () => {
-            map.getCanvas().style.cursor = '';
-        });
-});
