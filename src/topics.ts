019d70544b66ccfcb1882f52064067d346d8329c
Martin Nester <nesterms@dukes.jmu.edu>
2024-08-01 21:21:16 +0000

    moving things to their own files, and also reformatting with prettier

---
+++ src/topics.ts
@@ -0,0 +1,28 @@
+import * as ROSLIB from 'roslib';
+const ros = new ROSLIB.Ros({
+  url : 'ws://localhost:9090'
+});
+
+export const visual_path = new ROSLIB.Topic({
+	ros:ros,
+	name: '/visual_path',
+	messageType: 'visualization_msgs/msg/MarkerArray'
+});
+
+export const limited_pose = new ROSLIB.Topic({
+	ros:ros,
+	name: '/pcl_pose',
+	messageType: 'geometry_msgs/msg/PoseWithCovarianceStamped'
+});
+export const vehicle_state = new ROSLIB.Topic({
+	ros:ros,
+	name: '/vehicle_state',
+	messageType: 'navigation_interface/msg/VehicleState'
+});
+
+export const clicked_point = new ROSLIB.Topic({
+	ros : ros,
+	name : '/clicked_point',
+	messageType : 'geometry_msgs/msg/PointStamped'
+});
+
---
+++ src/transform.ts
@@ -0,0 +1,17 @@
+import { Matrix, inverse } from 'ml-matrix';
+
+let A = new Matrix([[ -0.00000156,   0.00001108, -78.86214758],
+ [ -0.00000849,  -0.00000137,  38.43388357],
+ [  0,           0,           1        ]]);
+let B = inverse(A);
+export function T(position){
+		const {x,y} = position;
+		const [x2,y2,z] = A.mmul(Matrix.columnVector([x,y,1])).to1DArray();
+		return [x2,y2];
+	};
+export function Y(lngLat){
+		const {lat,lng} = lngLat;
+		const [x2,y2,z] = B.mmul(Matrix.columnVector([lng,lat,1])).to1DArray();
+		return [x2,y2];
+	};
+
