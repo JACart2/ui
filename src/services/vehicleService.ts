581fa4bc65457b10851dac8b3a256f7e5ca3d1ec
Barrett Crusse <cvillebear@gmail.com>
2025-05-12 11:33:49 -0400

    Change default server IP, ignore env for now

--- src/services/vehicleService.ts
+++ src/services/vehicleService.ts
@@ -1,11 +1,13 @@
 console.log(import.meta.env);
 
-const DEFAULT_SERVER_IP =
-  import.meta.env.VITE_SERVER_IP ?? "http://172.28.71.175/";
+// const DEFAULT_SERVER_IP =
+//   import.meta.env.VITE_SERVER_IP ?? "http://10.147.17.17:8002/";
+const DEFAULT_SERVER_IP = "http://10.147.17.17:8002/";
 const VEHICLES_ENDPOINT = "api/vehicles/";
 
 export const vehicleService = {
-  SERVER_IP: import.meta.env.VITE_SERVER_IP ?? "http://172.28.71.175/",
+  // SERVER_IP: import.meta.env.VITE_SERVER_IP ?? "http://10.147.17.17:8002/",
+  SERVER_IP: "http://10.147.17.17:8002/",
   BASE_URL: DEFAULT_SERVER_IP + VEHICLES_ENDPOINT,
 
   setServerIP(address: string) {
