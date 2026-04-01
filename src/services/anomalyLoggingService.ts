585bba8276a1121a481b4362de015ebf4b6b6178
Luke B <luke.brenn04@gmail.com>
2026-04-01 15:46:37 -0400

    Log ROS connection and topic advertisement status
    Add logging for ROS connection and topic advertisement status.

--- src/services/anomalyLoggingService.ts
+++ src/services/anomalyLoggingService.ts
@@ -111,6 +111,8 @@ function publishText(params: {
   } as Partial<AnomalyMsg>);
 
   try {
+    console.log("ROS connected:", ros.isConnected);
+    console.log("Topic advertised:", ai_anomaly_logging.isAdvertised);
     ai_anomaly_logging.publish(message);
     console.log("[ai_anomaly_logging]", params.msg);
   } catch (err) {
